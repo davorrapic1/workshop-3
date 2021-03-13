81f153d5a2add5e31792e11b6e2e182647a2f4c0
Davor <rapic.davor@gmail.com>
2021-03-13 13:41:34 +0100

    init

---
+++ index.js
@@ -0,0 +1,47 @@
+// // //  var button = document.getElementById('dugme')
+// // // button.addEventListener('mouseover', (event) => {
+// // //    console.log(event);
+// // // })
+
+// // // button.addEventListener('click', (event) => {
+// // //     console.log(button.parentNode.nodeName)
+// // // })
+// // // const width = '300px';
+// // var body = document.getElementById('content');
+
+// // // const newButton = document.createElement('button');
+// // // newButton.innerHTML = ' prog. created button';
+// // // newButton.style.width = width
+// // // newButton.classList.add('dugme')
+// // // body.appendChild(newButton);
+// // const SWtxt = document.createElement('div');
+// // SWtxt.innerHTML = "<h1>Hello World!</h1>";
+// // document.body.appendChild(SWtxt);
+// // const newH1=document.createElement('mojH1')
+// // newH1.innerHTML = 'Ovo je moj prvi H1 tag'
+// // body.appendChild(newH1);
+
+// // // const element = '<h1 class="naslov">Bok</h1>'
+// // // body.innerHTML = element;
+
+// // async function getSWdata() {
+// //     return fetch('https://swapi.dev/api/people/3');
+// // }
+// // getSWdata().then(response =>  response.json()).then(data => {
+// // });
+// const date = new Date()
+// const ime = 'Davor'
+// const godine = 35
+// const grad = 'Poreč'
+// const SWtxt = document.createElement('div');
+// SWtxt.innerHTML = `${ime}`;
+// //SWtxt.innerHTML = "Moje ime je " + ime + " imam " + godine + " godine " + " i zivim u " + grad;
+// document.body.appendChild(SWtxt);
+
+// uzmi listu
+// napravis for loop na 5 elemenata kreiral <li> item </li>
+
+// primjer loop-a
+// for(var i = 0; i < 5; i++) {
+//     kod ide unutra
+// }
